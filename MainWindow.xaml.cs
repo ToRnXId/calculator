bb57b805efbce199ace1991f7a6c78d0e2d4bc8b
Mykhailo Guivan <tornxid123@gmail.com>
2024-03-24 17:00:07 +0100

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,8 +32,11 @@ namespace Calculator
         }
 
         string lblTextValue = null;
+        string digit = "";
         string digits = "0123456789";
         string mathSigns = "+-*/";
+        string temp = "";
+        string[] arrayValues = {};
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -45,36 +48,36 @@ namespace Calculator
                     CheckRepeatSign(stringButton);
                     break;
                 case "CE":
-                    lblText.Text = "0";
+                    txtblNumber.Text = "0";
                     break;
                 case "C":
-                    lblText.Text = "0";
-                    lblTextHistory.Text = null;
+                    txtblNumber.Text = "0";
+                    txtblResultat.Text = null;
                     break;
                 case "Erase":
-                    if (lblText.Text.Length > 0)
+                    if (txtblNumber.Text.Length >= 1)
                     {
-                        lblText.Text = lblText.Text.Remove(lblText.Text.Length - 1);
+                        txtblNumber.Text = txtblNumber.Text.Remove(txtblNumber.Text.Length - 1);
 
-                        if (lblTextHistory.Text.Contains("mathSigns"))
-                        {
-                            lblTextHistory.Text = lblText.Text;
-                        }
-                        lblTextHistory.Text = lblText.Text;
-                    } else {
-                        lblText.Text = "0";
+                        //if (lblTextHistory.Text.Contains("mathSigns"))
+                        //{
+                        //    lblTextHistory.Text = lblText.Text;
+                        //}
+                        txtblResultat.Text = txtblNumber.Text;
+                    } else if (txtblNumber.Text.Length == 0 || txtblNumber.Text == null){
+                        txtblNumber.Text = "0";
                     }
                     
                     break;
                 case "1/x":
-                    if (!stringButton.Contains("+-*/"))
+                    if (!stringButton.Contains(mathSigns))
                     {
-                        int digit = int.Parse(lblTextHistory.Text);
+                        int digit = int.Parse(txtblResultat.Text);
                         double x = 1 / digit;
-                        lblTextHistory.Text = x.ToString();
+                        txtblResultat.Text = x.ToString();
                     } else
                     {
-                        lblTextHistory.Text = "Error";
+                        txtblResultat.Text = "Error";
                     }
                     break;
                 case "x2":
@@ -82,21 +85,32 @@ namespace Calculator
                 case "sqrt2":
                     break;
                 case "/":
+                    if (!txtblResultat.Text.EndsWith("-"))
+                        txtblResultat.Text += txtblNumber.Text;
                     //CheckRepeatSign(stringButton);
-                    ChechPreviousSign(stringButton);
+                    CheckPreviousSign(stringButton);
                     break;
                 case "*":
-                    ChechPreviousSign(stringButton);
+                    if (!txtblResultat.Text.EndsWith("-"))
+                        txtblResultat.Text += txtblNumber.Text;
+                    CheckPreviousSign(stringButton);
                     //CheckRepeatSign(stringButton);
                     break;
                 case "-":
-                    ChechPreviousSign(stringButton);
+                    if (!txtblResultat.Text.EndsWith("-"))
+                        txtblResultat.Text += txtblNumber.Text;
+
+                    CheckPreviousSign(stringButton);
+                    
+                    //CheckPreviousSign(stringButton);
                     //CheckRepeatSign(stringButton);
                     //lblTextHistory.Text += stringButton;
                     //lblTextValue = "";
                     break;
                 case "+":
-                    ChechPreviousSign(stringButton);
+                    if (!txtblResultat.Text.EndsWith("-"))
+                        txtblResultat.Text += txtblNumber.Text;
+                    CheckPreviousSign(stringButton);
                     //CheckRepeatSign(stringButton);
                     break;
                 case "+/-":
@@ -105,20 +119,40 @@ namespace Calculator
                     CheckRepeatSign(stringButton);
                     break;
                 case "=":
-                    string value = new DataTable().Compute(lblTextHistory.Text, null).ToString(); 
-                    lblText.Text = null;
-                    lblTextHistory.Text = value;
+                    if (!txtblResultat.Text.Contains("="))
+                    {
+                        string value = new DataTable().Compute(txtblResultat.Text, null).ToString();
+                        txtblNumber.Text = value;
+                        temp = value;
+                        txtblResultat.Text += ("=" + value);
+                    }
+                    else
+                    {
+                        txtblResultat.Text = temp;
+                    }
                     break;
                 default:
-                    lblText.Text = lblText.Text.TrimStart('0');
-                    lblTextHistory.Text = lblTextHistory.Text.TrimStart('0');
-                    if (lblTextHistory.Text.EndsWith("-"))
+                    if (txtblResultat.Text.Contains('-'))
+                        txtblNumber.Text = stringButton;
+                    else         
+                        txtblNumber.Text += stringButton;
+                    if (txtblNumber.Text.Length > 1)
+                        txtblNumber.Text = txtblNumber.Text.TrimStart('0');
+                    else if (txtblNumber.Text.Length < 1)
+                        txtblNumber.Text = "0";
+                    //lblTextHistory.Text = lblTextHistory.Text.TrimStart('0');
+                    digit = stringButton;
+
+
+
+                    /*if (lblTextHistory.Text.EndsWith("-"))
                     {
                         lblText.Text = "";
                     }
+
                     lblTextValue = stringButton;
                     lblText.Text += lblTextValue;
-                    lblTextHistory.Text += stringButton;
+                    lblTextHistory.Text += stringButton;*/
                     break;
             }
 
@@ -137,24 +171,26 @@ namespace Calculator
 
         }
 
-        private void ChechPreviousSign(string sign)
+        private void CheckPreviousSign(string sign)
         {
-            lblTextValue = sign;
+            //lblTextValue = sign;
             int counter = 0;
+
             foreach (char el in mathSigns)
             {
                 string strElement = el.ToString();
-                if (lblTextHistory.Text.EndsWith(strElement))
+                if (txtblResultat.Text.EndsWith(strElement))
                 {
                     counter++;
-                    lblTextHistory.Text = lblTextHistory.Text.Remove(lblTextHistory.Text.Length - 1);
-                    lblTextHistory.Text += sign;
+                    txtblResultat.Text = txtblResultat.Text.Remove(txtblResultat.Text.Length - 1);
+                    txtblResultat.Text += sign;
+                    txtblNumber.Text = digit;
                 }
             }
 
             if (counter == 0)
             {
-                lblTextHistory.Text += sign;
+                txtblResultat.Text += sign;
             }
 
         }
@@ -162,12 +198,12 @@ namespace Calculator
         private void CheckRepeatSign(string sign)
         {
             lblTextValue = sign;
-            if (lblTextHistory.Text.EndsWith(mathSigns))
+            if (txtblResultat.Text.EndsWith(mathSigns))
             {
-                lblTextHistory.Text += 1;
+                txtblResultat.Text += 1;
             } else
             {
-                lblTextHistory.Text += sign;
+                txtblResultat.Text += sign;
             }
         }
     }
